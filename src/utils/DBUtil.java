e5aa5b30ea49dcac1537d9327419e9443388a0aa
keisuke.suzuki <mokyuzero.spirituals@icloud.com>
2020-02-11 08:26:40 +0900

    Add getAllTasks;

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "message_board";
+    private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
